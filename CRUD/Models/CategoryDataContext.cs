65481fbe6c4f852027bb6c4a21ea29a00ac0e4c9
bonnie.lin <bonnie.lin@ecpay.com.tw>
2021-12-14 11:15:26 +0800

    加入專案檔案。

---
+++ CRUD/Models/CategoryDataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRUD.Models
+{
+    public class CategoryDataContext
+    {
+       static string strConn = "intial catalog=northwind;userid=sa;passwoed=P@ssw0rd;data souce=.";
+
+       //讀取所有產品分類資料
+       public static List<Category> LoadCategories()
+        {
+            List<Category> categories = new List<Category>();
+
+        }
+
+        
+    }
+}
